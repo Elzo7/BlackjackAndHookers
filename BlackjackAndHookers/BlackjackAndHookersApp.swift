c7e0c4ff9ee9d58c37503eb21af167ffdf66a2e2
Damian Traczuk <damiantra7@gmail.com>
2023-12-12 16:27:32 +0100

    Initial Commit

---
+++ BlackjackAndHookers/BlackjackAndHookersApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlackjackAndHookersApp.swift
+//  BlackjackAndHookers
+//
+//  Created by student on 12/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct BlackjackAndHookersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
